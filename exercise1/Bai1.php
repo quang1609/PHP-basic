67372acf0eaed6f84c2f886e7cdf4c9a794f529a
Pham Quang <quangbn1609@gmail.com>
2021-09-23 09:44:46 +0700

    fix coding convention

--- exercise1/Bai1.php
+++ exercise1/Bai1.php
@@ -1,6 +1,6 @@
 <?php
-function checkValidString($str) 
-{
+ function checkValidString($str) 
+ {
     $val1 = strpos($str, 'book');
     $val2 = strpos($str, 'restaurant');
     if ($val1 > 0 && $val2 == false) {
@@ -10,30 +10,31 @@ function checkValidString($str)
     } else {
         return false;
     }
-}
+ }
 
-function checkResult($str) 
-{
+ function checkResult($str)
+ {
 	if (checkValidString($str) === false) {
 	    echo 'Chuỗi không hợp lệ!!';
     } else {
 	    $sentence = substr_count($str, '.');
 	    echo 'Chỗi hợp lệ. Chuỗi bao gồm ' . $sentence . ' câu';
     }
-}
+ }
 
-function checkFile($file) 
-{
+ function checkFile($file) 
+ {
     if (file_exists($file)) {
-    	$f = fopen($file, 'r');
-    	$content = fread($f, filesize($file));
+    	$fileOpen = fopen($file, 'r');
+    	$content = fread($fileOpen, filesize($file));
     	checkResult($content);
     } else {
     	echo 'Tệp không tồn tại!!';
     }
-}
+ }
 
-//Check File
-checkFile('file1.txt');//Chuỗi hợp lệ và bao gồm 4 câu
-echo '<br>';
-checkFile('file2.txt');//Chuỗi không hợp lệ
+ //Check File
+ checkFile('file1.txt');//Chuỗi hợp lệ và bao gồm 4 câu
+ echo '<br>';
+ checkFile('file2.txt');//Chuỗi không hợp lệ
+?>
